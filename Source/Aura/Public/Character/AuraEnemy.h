8d3fd4e8d63b6ad20cfcf8d2fff7e3673b3fcb88
Felipe Viana <felipetiburcioviana@gmail.com>
2023-12-12 22:48:08 -0800

    Post Process HighLight

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -17,9 +17,13 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 
 public:
 
+	AAuraEnemy();
+
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
 
 	UPROPERTY(BlueprintReadOnly)
 	bool bHighlighted = false;
 };
+
+
