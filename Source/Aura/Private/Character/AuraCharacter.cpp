4e8dbd51aa3ae535e5a82492125304584de935f7
Felipe Viana <felipetiburcioviana@gmail.com>
2024-01-13 20:26:45 -0800

    Initialize Attributes with Gameplay Effects

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -53,6 +53,8 @@ void AAuraCharacter::InitAbilityActorInfo()
             AuraHUD->InitOverlay(AuraPlayerController, AuraPlayerState, AbilitySystemComponent, AttributeSet);
        }
     }
+
+    InitializePrimaryAttributes();
     
 
 }
