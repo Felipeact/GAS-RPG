e52b02ef6e3faa25abb65c5e5a5f9012a2245151
Felipe Viana <felipetiburcioviana@gmail.com>
2024-01-22 22:15:26 -0800

    fixed vigor bugs

--- Source/Aura/Private/AbilitySystem/ModMagCalc/MMC_MaxHealth.cpp
+++ Source/Aura/Private/AbilitySystem/ModMagCalc/MMC_MaxHealth.cpp
@@ -9,10 +9,25 @@ UMMC_MaxHealth::UMMC_MaxHealth()
     VigorDef.AttributeToCapture = UAuraAttributeSet::GetVigorAttribute();
     VigorDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
     VigorDef.bSnapshot = false;
+
+    RelevantAttributesToCapture.add(VigorDef);
 }
 
 
 float UMMC_MaxHealth::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec & Spec) const
 {
+    // Gather tags from source and target
+    const FGameplayTagContainer* SourceTag = Spec.CapturedSourceTags.GetAggregatedTags();
+    const FGameplayTagContainer* TargetTag = Spec.CapturedTargetTags.GetAggregatedTags();
+
+    FAggregatorEvaluateParameters EvaluationParameters; 
+    EvaluationParameters.SourceTags = SourceTag;
+    EvaluationParameters.TargetTags = TargetTag;
+
+    float Vigor  = 0.f;
+    GetCapturedAttributeMagnitude(VigorDef, Spec, EvaluationParameters, Vigor);
+    Vigor = FMath::Max<float>(Vigor, 0.f);
+
+    
 
 }
