d954ddf1cce01cbc4e99ef62f3fa894d3d7e1953
Felipe Viana <felipetiburcioviana@gmail.com>
2024-02-11 20:49:24 -0800

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,16 @@
+// Copyright  ByViana
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+
+}
+
+void UAttributeMenuWidgetController::BroadCastInitialValues()
+{
+
+}
+
+
---
+++ Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -0,0 +1,22 @@
+// Copyright  ByViana
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+#include "AttributeMenuWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAttributeMenuWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+	
+public:
+
+	virtual void BindCallbacksToDependencies() override;
+
+	virtual void BroadCastInitialValues() override;
+};
