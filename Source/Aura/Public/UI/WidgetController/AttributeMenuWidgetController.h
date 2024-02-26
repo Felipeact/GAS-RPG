fbd0e9629e3f3fca6fb7f4b6b8645bbe4cea55fb
Felipe Viana <felipetiburcioviana@gmail.com>
2024-02-25 19:36:18 -0800

    Responding to Attribute Changes

--- Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -7,7 +7,9 @@
 #include "AttributeMenuWidgetController.generated.h"
 
 class UAttributeInfo;
-struct FAuraAttributeInf;
+struct FGameplayTag;
+struct FGameplayAttribute;
+struct FAuraAttributeInfo;
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAttributeInfoSignature, const FAuraAttributeInfo&, Info);
 
 /**
@@ -24,6 +26,7 @@ public:
 	virtual void BindCallbacksToDependencies() override; 
 	virtual void BroadcastInitialValues() override;
 
+	
 	UPROPERTY(BlueprintAssignable, Category = "GAS| Attributes")
 	FAttributeInfoSignature AttributeInfoDelegate;
 
@@ -31,5 +34,9 @@ protected:
 
 	UPROPERTY(EditDefaultsOnly)
 	TObjectPtr<UAttributeInfo> AttributeInfo;
+
+private:
+
+	void BroadcastAttributeInfo(const FGameplayTag& AttributeTag, const FGameplayAttribute& Attribute) const;
 
 };
